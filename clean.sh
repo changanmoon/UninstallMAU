8c27703d1a766b1ec1403de214b205e3d53fad70
Tiffany Feng <46277006+changanmoon@users.noreply.github.com>
2022-12-15 21:00:32 +0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -6,36 +6,36 @@ sudo -S id -u
 echo "" | awk '{fflush()}'
 
 # Define a universal function to delete file/folder under sudo privileges
-function safe_delete() {
+function delete() {
     # use "test -e" command to check if a file/folder exists
     sudo test -e "$1" && sudo rm -rf "$1"
 }
 
 # Delete application supoort
-safe_delete /Library/PrivilegedHelperTools/com.microsoft.autoupdate.helper
-safe_delete /Library/Application\ Support/Microsoft/MAU2.0
-safe_delete ~/Library/Application\ Support/Microsoft\ AutoUpdate
+delete /Library/PrivilegedHelperTools/com.microsoft.autoupdate.helper
+delete /Library/Application\ Support/Microsoft/MAU2.0
+delete ~/Library/Application\ Support/Microsoft\ AutoUpdate
 
 # Delete preferences
-safe_delete /Library/Preferences/com.microsoft.autoupdate2.plist
-safe_delete ~/Library/Preferences/com.microsoft.autoupdate2.plist
-safe_delete ~/Library/Preferences/com.microsoft.autoupdate.fba.plist
+delete /Library/Preferences/com.microsoft.autoupdate2.plist
+delete ~/Library/Preferences/com.microsoft.autoupdate2.plist
+delete ~/Library/Preferences/com.microsoft.autoupdate.fba.plist
 
 # Delete containers
-safe_delete ~/Library/Group\ Containers/UBF8T346G9.ms
-safe_delete ~/Library/Group\ Containers/UBF8T346G9.office
+delete ~/Library/Group\ Containers/UBF8T346G9.ms
+delete ~/Library/Group\ Containers/UBF8T346G9.office
 
 # Delete cache and log files
-safe_delete /private/var/db/receipts/com.microsoft.package.Microsoft_AutoUpdate.app.bom
-safe_delete /private/var/db/receipts/com.microsoft.package.Microsoft_AutoUpdate.app.plist
-safe_delete /Library/Logs/Microsoft
-safe_delete ~/Library/Caches/com.microsoft.autoupdate.fba
-safe_delete ~/Library/Caches/com.microsoft.autoupdate2
-safe_delete ~/Library/HTTPStorages/com.microsoft.autoupdate2
+delete /private/var/db/receipts/com.microsoft.package.Microsoft_AutoUpdate.app.bom
+delete /private/var/db/receipts/com.microsoft.package.Microsoft_AutoUpdate.app.plist
+delete /Library/Logs/Microsoft
+delete ~/Library/Caches/com.microsoft.autoupdate.fba
+delete ~/Library/Caches/com.microsoft.autoupdate2
+delete ~/Library/HTTPStorages/com.microsoft.autoupdate2
 
 # Delete startup items
-safe_delete /Library/LaunchDaemons/com.microsoft.autoupdate.helper.plist
-safe_delete /Library/LaunchAgents/com.microsoft.update.agent.plist
+delete /Library/LaunchDaemons/com.microsoft.autoupdate.helper.plist
+delete /Library/LaunchAgents/com.microsoft.update.agent.plist
 
 # Ending
 sudo killall Finder; killall Dock
