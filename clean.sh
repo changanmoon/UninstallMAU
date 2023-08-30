01a9965551ca71588b29d4d87437211293d2dfa8
Tiffany Fung <46277006+changanmoon@users.noreply.github.com>
2023-08-30 13:32:39 +0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2022 Tiffany Feng
+# Copyright 2022-2023 Tiffany Feng
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,27 +17,27 @@
 sudo -S id -u
 echo "" | awk '{fflush()}'
 
-# Define a universal function to delete file/folder under sudo privileges
+# Define a universal function to remove file/folder under sudo privileges
 function delete() {
     # use "test -e" command to check if a file/folder exists
     sudo test -e "$1" && sudo rm -rf "$1"
 }
 
-# Delete application support folders
+# Remove application support folders
 delete /Library/PrivilegedHelperTools/com.microsoft.autoupdate.helper
 delete /Library/Application\ Support/Microsoft/MAU2.0
 delete ~/Library/Application\ Support/Microsoft\ AutoUpdate
 
-# Delete preferences
+# Remove preferences
 delete /Library/Preferences/com.microsoft.autoupdate2.plist
 delete ~/Library/Preferences/com.microsoft.autoupdate2.plist
 delete ~/Library/Preferences/com.microsoft.autoupdate.fba.plist
 
-# Delete containers
+# Remove containers
 delete ~/Library/Group\ Containers/UBF8T346G9.ms
 delete ~/Library/Group\ Containers/UBF8T346G9.office
 
-# Delete cache and log files
+# Remove cache and log files
 delete /private/var/db/receipts/com.microsoft.package.Microsoft_AutoUpdate.app.bom
 delete /private/var/db/receipts/com.microsoft.package.Microsoft_AutoUpdate.app.plist
 delete /Library/Logs/Microsoft
@@ -45,7 +45,7 @@ delete ~/Library/Caches/com.microsoft.autoupdate.fba
 delete ~/Library/Caches/com.microsoft.autoupdate2
 delete ~/Library/HTTPStorages/com.microsoft.autoupdate2
 
-# Delete startup items
+# Remove startup items
 delete /Library/LaunchDaemons/com.microsoft.autoupdate.helper.plist
 delete /Library/LaunchAgents/com.microsoft.update.agent.plist
 
